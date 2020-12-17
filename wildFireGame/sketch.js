efc25022a37dada273ef8de1c380ff4e89ad787f
elsaB2023 <elsabosemark@gmail.com>
2020-12-17 08:13:58 -0800

    set fire first round

--- wildFireGame/sketch.js
+++ wildFireGame/sketch.js
@@ -25,7 +25,7 @@ function setup() {
 	wH = windowHeight;
 	gColor = color(140, 193, 82);
 	grColor = color(160, 212, 104);
-	fireYear = floor(random(3,6));
+	fireYear = 3;
 	let moneyX;
 	moneyX = wX - 50;
 	for (let i = 0; i < 6; i++) {
